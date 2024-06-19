1bb6ba1b34ac0aba43b6d0404fdc1f54bf019af2
Raoul de Grunt <>
2024-06-19 08:27:30 +0200

    19/06/2024 - Make credentials location configurable
    config.php
    
    - Added FRAMEWORK_GOOGLE_CREDENTIALS
    
    ServiceAccountBase.php - 1.0.1
    
    - Use FRAMEWORK_GOOGLE_CREDENTIALS

--- Framework/Google/Baseclasses/ServiceAccountBase.php
+++ Framework/Google/Baseclasses/ServiceAccountBase.php
@@ -14,7 +14,7 @@ use Google_Client;
  * @author Raoul de Grunt
  * @package Framework\Google
  * @link https://github.com/googleapis/google-api-php-client/tree/main?tab=readme-ov-file#authentication-with-service-accounts
- * @version 1.0.0
+ * @version 1.0.1
  */
 abstract class ServiceAccountBase
 {
@@ -24,6 +24,6 @@ abstract class ServiceAccountBase
     function __construct()
     {
         $this->client = new Google_Client();
-        $this->client->setAuthConfig(__DIR__ . '/../Credentials/credentials.json');
+        $this->client->setAuthConfig(FRAMEWORK_GOOGLE_CREDENTIALS . '/credentials.json');
     }
 }
